2d35fe91345043fef023b14e53f3a815cd21db2f
gusttavares97 <gustavotavares018@gmail.com>
2023-01-20 12:00:44 -0300

    fetch bitcoin refatorado

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
 import FetchAnimais from './modules/fetch-animais.js';
-import initFetchBitcoin from './modules/fetch-bitcoin.js';
+import FetchBitcoin from './modules/fetch-bitcoin.js';
 import initAccordion from './modules/accordion.js';
 
 const TabNav = new TabNav('[data-tab="menu"] li','[data-tab="content"] section');
@@ -21,6 +21,8 @@ initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
-initFetchBitcoin();
 
 FetchAnimais('../../animaisapi.json', '.numeros-grid');
+
+FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
