926bd113982ce2bc15eb584b7285fb44acf336e8
gusttavares97 <gustavotavares018@gmail.com>
2023-01-31 10:52:50 -0300

    finalizacao do site

--- js/modules/scroll-anima.js
+++ js/modules/scroll-anima.js
@@ -1,54 +1,48 @@
-export default class SrollAnima {
-  contructor(sections){
-  this.sections = document.querySelectorAll('sections');
- this.windowMetade = window.innerHeight * 0.6;
- 
- this.animaScroll= this.amimaScroll.bind(this)
+import debounce from './debounce.js';
+
+export default class ScrollAnima {
+  constructor(sections) {
+    this.sections = document.querySelectorAll(sections);
+    this.windowMetade = window.innerHeight * 0.6;
+
+    this.checkDistance = debounce(this.checkDistance.bind(this), 50);
   }
-  getDistance(){
-    this.distance =[...this.sections].map.forEach(section =>{
-      const offSet = sections.offsetTop;
-     return{
-      element: section,
-      offSet: math.flor(offSet) - this.windowMetade,
-     };
+
+  // Pega a distância de cada item em relação
+  // ao topo do site
+  getDistance() {
+    this.distance = [...this.sections].map((section) => {
+      const offset = section.offsetTop;
+      return {
+        element: section,
+        offset: Math.floor(offset - this.windowMetade),
+      };
     });
   }
 
-  checkDistance(){
-    this.distance.forEach((item)=>{
-      if(window.pageXOffset > item.offSet){
-        item.element.classList.add('ativo');   
-      }else if(item.element.classList.contains('ativo')){
+  // Verifica a distância em cada objeto
+  // em relação ao scroll do site
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.pageYOffset > item.offset) {
+        item.element.classList.add('ativo');
+      } else if (item.element.classList.contains('ativo')) {
         item.element.classList.remove('ativo');
       }
     });
   }
- 
-animaScroll() {
-    this.sections.forEach((section) => {
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = (sectionTop - this.windowMetade) < 0;
-      if (isSectionVisible) {
-        section.classList.add('ativo');
-      } else if (section.classList.contains('ativo')) {
-        section.classList.remove('ativo');
-      }
-    });
-  }
 
-  init(){
-    if(this.sections.length){
-    this.getDistance()
-    this.animaScroll()
-    window.addEventListener('scroll', this.checkDistance);
+  init() {
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener('scroll', this.checkDistance);
     }
     return this;
   }
-stop(){
-  window.removeListener('scroll', this.checkDistance);
 
-
-}
-
+  // Remove o event de scroll
+  stop() {
+    window.removeEventListener('scroll', this.checkDistance);
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,33 +1,47 @@
-import initScrollSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
+import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
-import initModal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
-import initDropdownMenu from './modules/dropdown-menu.js';
-import initMenuMobile from './modules/menu-mobile.js';
-import initFuncionamento from './modules/funcionamento.js';
-import FetchAnimais from './modules/fetch-animais.js';
-import FetchBitcoin from './modules/fetch-bitcoin.js';
-import initAccordion from './modules/accordion.js';
-import scrollAnima from './modules/scroll-anima.js';
+import Modal from './modules/modal.js';
+import Tooltip from './modules/tooltip.js';
+import DropdownMenu from './modules/dropdown-menu.js';
+import MenuMobile from './modules/menu-mobile.js';
+import Functionamento from './modules/funcionamento.js';
+import fetchAnimais from './modules/fetch-animais.js';
+import fetchBitcoin from './modules/fetch-bitcoin.js';
+import ScrollAnima from './modules/scroll-anima.js';
+import SlideNav from './modules/slide.js';
 
-const TabNav = new TabNav('[data-tab="menu"] li','[data-tab="content"] section');
-TabNav.init();
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
-const scollAnima =new ScrollAnima('[data-anime="scroll"]', )
-scollAnima.init()
-scollAnima.stop()
+const accordion = new Accordion('[data-anime="accordion"] dt');
+accordion.init();
 
+const tabNav = new TabNav('[data-tab="menu"] li', '[data-tab="content"] section');
+tabNav.init();
 
+const modal = new Modal('[data-modal="abrir"]', '[data-modal="fechar"]', '[data-modal="container"]');
+modal.init();
 
-initScrollSuave();
-initAccordion();
-initModal();
-initTooltip();
-initDropdownMenu();
-initMenuMobile();
-initFuncionamento();
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
 
-FetchAnimais('../../animaisapi.json', '.numeros-grid');
+const scrollAnima = new ScrollAnima('[data-anime="scroll"]');
+scrollAnima.init();
 
-FetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+const dropdownMenu = new DropdownMenu('[data-dropdown]');
+dropdownMenu.init();
 
+const menuMobile = new MenuMobile('[data-menu="button"]', '[data-menu="list"]');
+menuMobile.init();
+
+const funcionamento = new Functionamento('[data-semana]', 'aberto');
+funcionamento.init();
+
+fetchAnimais('./animaisapi.json', '.numeros-grid');
+
+fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
+
+const slide = new SlideNav('.slide', '.slide-wrapper');
+slide.init();
+slide.addControl('.custom-controls');
